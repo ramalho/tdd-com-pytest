bf844b7dc851b84f874b8b9e49acd122fc579a5d
Luciano Ramalho <luciano@ramalho.org>
2018-05-25 17:39:12 -0300

    final do dojo na Python Nordeste 2018

---
+++ sinais/sinais.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+
+def analisar_documento(documento):
+    res = []
+    for linha in documento:
+        res.append(analisar_linha(linha))
+    return res
+
+def analisar_linha(linha):
+    codigo, nome = linha.split(';')[:2]
+    return codigo, nome
+
+def buscar(consulta, registros):
+    achados = []
+    consulta = set(consulta.upper().split(' '))
+    for codigo, nome in registros:
+        tokens = set(nome.split(' '))
+        if consulta <= tokens:
+            achados.append((codigo, nome))
+    return achados
+
+def formatar(resultados):
+    formatados = [f'U+{codigo}\t{chr(int(codigo, 16))}\t{nome}'
+                  for codigo, nome in resultados]
+    return formatados
+
+def main():
+    text_busca = ' '.join(sys.argv[1:])
+    with open ('UnicodeData.txt') as documento:
+        resultados = formatar(buscar(text_busca, analisar_documento(documento)))
+
+    print('\n'.join(resultados))
+
+if __name__ == '__main__':
+    main()
+    
