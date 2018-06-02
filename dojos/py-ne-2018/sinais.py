edae805ddf2267d8e08eea6ab344242217f52043
Luciano Ramalho <luciano@ramalho.org>
2018-06-02 16:59:43 -0300

    passando flake8 no dojo da PyNE

--- dojos/py-ne-2018/sinais.py
+++ dojos/py-ne-2018/sinais.py
@@ -2,15 +2,18 @@
 
 import sys
 
+
 def analisar_documento(documento):
     res = []
     for linha in documento:
         res.append(analisar_linha(linha))
     return res
 
+
 def analisar_linha(linha):
     codigo, nome = linha.split(';')[:2]
     return codigo, nome
+
 
 def buscar(consulta, registros):
     achados = []
@@ -21,17 +24,21 @@ def buscar(consulta, registros):
             achados.append((codigo, nome))
     return achados
 
+
 def formatar(resultados):
     formatados = [f'U+{codigo}\t{chr(int(codigo, 16))}\t{nome}'
                   for codigo, nome in resultados]
     return formatados
 
+
 def main(args):
     text_busca = ' '.join(args)
-    with open ('UnicodeData.txt') as documento:
-        resultados = formatar(buscar(text_busca, analisar_documento(documento)))
+    with open('UnicodeData.txt') as documento:
+        resultados = formatar(buscar(text_busca,
+                                     analisar_documento(documento)))
 
     print('\n'.join(resultados))
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- dojos/py-ne-2018/test_sinais.py
+++ dojos/py-ne-2018/test_sinais.py
@@ -9,10 +9,12 @@ def documento():
     return io.StringIO('0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;\n'
                        '0042;LATIN CAPITAL LETTER B;Lu;0;L;;;;;N;;;;0062;\n')
 
+
 def test_analisar_documento(documento):
     resultado = analisar_documento(documento)
     assert resultado == [('0041', 'LATIN CAPITAL LETTER A'),
                          ('0042', 'LATIN CAPITAL LETTER B')]
+
 
 def test_analisar_linha():
     linha = '0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;'
@@ -20,40 +22,49 @@ def test_analisar_linha():
     assert codigo == '0041'
     assert nome == 'LATIN CAPITAL LETTER A'
 
+
 def test_buscar_latin(documento):
     busca = buscar('LATIN', analisar_documento(documento))
     assert busca == [('0041', 'LATIN CAPITAL LETTER A'),
                      ('0042', 'LATIN CAPITAL LETTER B')]
 
+
 def test_buscar_B(documento):
     busca = buscar('B', analisar_documento(documento))
     assert busca == [('0042', 'LATIN CAPITAL LETTER B')]
+
 
 def test_buscar_b(documento):
     busca = buscar('b', analisar_documento(documento))
     assert busca == [('0042', 'LATIN CAPITAL LETTER B')]
 
+
 def test_buscar_a(documento):
     busca = buscar('a', analisar_documento(documento))
     assert busca == [('0041', 'LATIN CAPITAL LETTER A')]
+
 
 def test_buscar_string_vazia(documento):
     busca = buscar('', analisar_documento(documento))
     assert busca == []
 
+
 def test_buscar_varias(documento):
     busca = buscar('B LETTER', analisar_documento(documento))
     assert busca == [('0042', 'LATIN CAPITAL LETTER B')]
 
+
 def test_formatar_resultado():
     esperado = ['U+0041\tA\tLATIN CAPITAL LETTER A']
     assert esperado == formatar([('0041', 'LATIN CAPITAL LETTER A')])
+
 
 def test_formatar_resutado_varios():
     esperado = ['U+0041\tA\tLATIN CAPITAL LETTER A',
                 'U+0042\tB\tLATIN CAPITAL LETTER B']
     assert esperado == formatar([('0041', 'LATIN CAPITAL LETTER A'),
                                  ('0042', 'LATIN CAPITAL LETTER B')])
+
 
 def test_main(capsys):
     main(['registered'])
